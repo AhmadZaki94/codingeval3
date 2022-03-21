6062cba5bbec79a93fd03cb44f84fa15ed302885
Ahmad Zaki <shaikhzaki8@gmail.com>
2022-03-21 15:49:48 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,6 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
-import { BookDetailsPage } from './Components/BookDetailsPage/BookDetailsPage'
-import { BookCard } from './components/BookCard/BookCard'
-import { Home } from './Components/Home/Home'
-import { Navbar } from './components/Navbar'
-import { NotFound } from './components/NotFound'
-import { Section } from './components/NotFound'
-import { SortAndFilterButtons } from './components/SortAndFilterButtons'
 
 function App() {
   

c660507ab4edc9ce964af1185f45d626e5201673
Ahmad Zaki <shaikhzaki8@gmail.com>
2022-03-21 15:40:11 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { BookDetailsPage } from './Components/BookDetailsPage/BookDetailsPage'
+import { BookCard } from './components/BookCard/BookCard'
+import { Home } from './Components/Home/Home'
 
 function App() {
   
 
   return (
     <div className="App">
-      
+        <BookDetailsPage/>
+        <BookCard/>
+        <Home/>
+        <Navbar/>
+        <NotFound/>
+        <Section/>
+        <SectionAndFilterButtons/>
     </div>
   )
 }
